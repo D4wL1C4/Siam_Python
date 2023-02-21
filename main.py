4df6005e0b162410352e9416be5eea96856263fc
An1one <96392343+An1onegit@users.noreply.github.com>
2023-02-21 13:20:13 +0100

    pygame initi

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-import csv 
+import csv
 import pygame
 import random
+
+from siam_game.ressources import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+
+print(square)
+
+
+def MainGame():
+    run = True
+    fps = 60
+    while run:
+        clock.tick(fps)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
+
+        screen.blit(rhino, (50,50))
+
+MainGame()
