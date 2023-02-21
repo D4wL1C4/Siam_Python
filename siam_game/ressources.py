6cc0e498d13965bf8930cf3e8d9fc84a77802a63
D4wL1C4 <110841963+D4wL1C4@users.noreply.github.com>
2023-02-21 14:15:56 +0100

    +montagne ; correction de taille case

--- siam_game/ressources.py
+++ siam_game/ressources.py
@@ -2,15 +2,18 @@ import pygame
 
 
 #definition de la taille de l'ecran
-width = 800
-height = 800
+screen_width = 800
+screen_height = 800
 
 #le nombre de lignes et de colonnes
 row = 5
 col = 5 
 
-#le nombre de case = la largeur divisé par la hauteur (// : division euclidienne)
-square = width // height
+#nombre cases 
+case_number = row * col
+
+#taille case = côté divisé par le nombre de colonne ou ligne(= car carré) (// : division euclidienne)
+square = screen_width // row
 
 #definition des couleurs des cases du plateau
 l_brown = (225, 128, 23)
@@ -19,11 +22,12 @@ green = (124, 176, 62)
 #Charger les pieces
 #Rhinocéros
 rhino = pygame.image.load("siam_game/sources/rhino.png")
-#rhino = pygame.transform.scale(rhino, (square, square))
+rhino = pygame.transform.scale(rhino, (square, square))
 
 #elephant
 eleph = pygame.image.load("siam_game/sources/eleph.png")
 eleph = pygame.transform.scale(eleph, (square, square))
 
 #montagne
-#mont = pygame.image.load("siam_game/sources/")
+mont = pygame.image.load("siam_game/sources/montagne.png")
+mont = pygame.transform.scale(mont, (square, square))
