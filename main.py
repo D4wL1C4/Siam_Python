fe973df865b7762df9f5f049f77efb13492c4148
An1one <96392343+An1onegit@users.noreply.github.com>
2023-02-24 12:23:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import pygame
 import random
 
 from siam_game.ressources import *
+from siam_game.pieces import *
+from siam_game.plateau import drawPlate
 
 pygame.init()
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-print(square)
+rhino_rect = rhino.get_rect()
 
+
+og_pos = 250
 
 def MainGame():
     run = True
@@ -23,12 +27,19 @@ def MainGame():
             if event.type == pygame.QUIT:
                 run = False
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN: #calcule la position du curseur
+                if pygame.mouse.get_pos()[0] > 250 and pygame.mouse.get_pos()[0] < 750 and pygame.mouse.get_pos()[1] > 150 and pygame.mouse.get_pos()[1] < 850:
+                    mousePos = pygame.mouse.get_pos()
+                    print(int((mousePos[0] - 250)/100), int((mousePos[1] - 150)/100)) # transforme le resultat en index de colonnes / lignes (ex : 0;1 - 3;4 etc...)
+                if rhino_rect.collidepoint():
+                    print("something")
 
-        screen.blit(rhino, (150,50))
-        screen.blit(rhino, (300,50))
-        screen.blit(rhino, (450,50))
-        screen.blit(eleph,(150,650))
-        screen.blit(eleph,(300,650))
-        screen.blit(eleph,(450,650))
+
+        drawPlate(screen, l_brown, green, 250, 250, square, square) #Plateau
+        screen.blit(rhino, (250, 650))
+        
+
+
 
 MainGame()
+
