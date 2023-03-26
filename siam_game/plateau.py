32a34df9f4deb752fe29140686fa47ff7cabb590
An1one <96392343+An1onegit@users.noreply.github.com>
2023-03-26 16:12:58 +0200

    ajout tableau virtuel representatif plateau de jeu

--- siam_game/plateau.py
+++ siam_game/plateau.py
@@ -1,9 +1,17 @@
 import pygame
 from siam_game.ressources import *
 
+plateau = [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0]
+    ]
 
 def drawPlate(surface, color, colorbis, posX, posY, width, height):
-    
     for i in range(row):
         if i != 0:
             posX += square
@@ -18,5 +26,14 @@ def drawPlate(surface, color, colorbis, posX, posY, width, height):
                 pygame.draw.rect(surface, color, (posX, posY + square*k, width, height))
             else:
                 pygame.draw.rect(surface, colorbis, (posX, posY  + square*k, width, height))
-              
 
+
+    # print("_____Plateau_____")
+    # print(plateau[0])
+    # print(plateau[1])
+    # print(plateau[2])
+    # print(plateau[3])
+    # print(plateau[4])
+    # print(plateau[5])
+    # print(plateau[6])
+    # print("_________________")    
