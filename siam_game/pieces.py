b8fc0ccd4b867bfaba9c789dc577d05f60d7af84
An1one <96392343+An1onegit@users.noreply.github.com>
2023-03-30 17:37:58 +0200

    Update pieces.py

--- siam_game/pieces.py
+++ siam_game/pieces.py
@@ -3,7 +3,10 @@ from siam_game.plateau import *
 from siam_game.ressources import *
 
 class Rhino:
-    def __init__(self, _screen, posX : int, posY : int, image, name : str):
+    previousPos = (0,0)
+    newPos = (0,0)
+
+    def __init__(self, _screen, posX : int, posY : int, image, name : str, rotation):
         self.posX = posX
         self.posY = posY
         self.image = pygame.transform.scale(image, (square, square))
@@ -12,6 +15,7 @@ class Rhino:
         self.selected = False
         self.canPlace = True 
         self.name = str(name)
+        self.rotation = rotation
 
     def Update(self):
         mousePos = pygame.mouse.get_pos()
@@ -24,6 +28,7 @@ class Rhino:
         mousePos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(mousePos) and self.selected == False:
+            self.previousPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
             print(f"click sur {self.name}")
             self.selected = True
         
@@ -31,18 +36,59 @@ class Rhino:
         mousePos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(mousePos) and self.selected == True:
-            self.selected = False
-            self.rect.center = 250, 150
-            self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
-            self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
-            print(f"reclick sur {self.name}")
-    
+            self.newPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
+            print(plateau[self.newPos[1]][self.newPos[0]])
+            if abs(self.newPos[0] - self.previousPos[0]) <= 1 and abs(self.newPos[1] - self.previousPos[1]) <= 1 and plateau[self.newPos[1]][self.newPos[0]] == 0:
+                if self.rotation == 0 and self.newPos[1] == self.previousPos[1] + 1:
+                    self.selected = False
+                    self.rect.center = 250, 150
+                    self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+                    self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                    print(f"reclick sur {self.name}")
+                elif self.rotation == 90 and self.newPos[0] == self.previousPos[0] + 1:
+                    self.selected = False
+                    self.rect.center = 250, 150
+                    self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+                    self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                    print(f"reclick sur {self.name}")
+                elif self.rotation == -90 and self.newPos[0] == self.previousPos[0] - 1:
+                    self.selected = False
+                    self.rect.center = 250, 150
+                    self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+                    self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                    print(f"reclick sur {self.name}")
+                elif self.rotation == 180 and self.newPos[1] == self.previousPos[1] - 1:
+                    self.selected = False
+                    self.rect.center = 250, 150
+                    self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+                    self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                    print(f"reclick sur {self.name}")
+                else:
+                    self.selected = False
+                    self.posX = (self.previousPos[0]*100)+250
+                    self.posY = (self.previousPos[1]*100)+150
+                    self.rect.center = self.posX, self.posY
+                    self.newPos = (self.previousPos)
+                    print("Else")
+        
     def move(self, x, y):
         if self.selected == True:
             self.posX = x - square/2
             self.posY = y - square / 2
             self.rect.center = x, y
-            
+
+    def rotate(self, degres):
+        mousePos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mousePos):
+            self.image = pygame.transform.rotate(self.image, degres)
+            self.rotation += degres
+
+
+
+
+
+
 class Montagne:
     def __init__(self, _screen, posX : int, posY : int, image, name : str):
         self.posX = posX
@@ -56,17 +102,17 @@ class Montagne:
         plateau[int((self.posY - 150)//100)][int((self.posX - 250)//100)] = 2
 
 
-rhino_1 = Rhino(screen, 250, 750, rhino, "rhino_1")
-rhino_2 = Rhino(screen, 350, 750, rhino, "rhino_2")
-rhino_3 = Rhino(screen, 450, 750, rhino, "rhino_3")
-rhino_4 = Rhino(screen, 550, 750, rhino, "rhino_4")
-rhino_5 = Rhino(screen, 650, 750, rhino, "rhino_5")
+rhino_1 = Rhino(screen, 250, 750, rhino, "rhino_1", 0)
+rhino_2 = Rhino(screen, 350, 750, rhino, "rhino_2", 0)
+rhino_3 = Rhino(screen, 450, 750, rhino, "rhino_3", 0)
+rhino_4 = Rhino(screen, 550, 750, rhino, "rhino_4", 0)
+rhino_5 = Rhino(screen, 650, 750, rhino, "rhino_5", 0)
 
-eleph_1 = Rhino(screen, 250, 150, eleph, "eleph_1")
-eleph_2 = Rhino(screen, 350, 150, eleph, "eleph_2")
-eleph_3 = Rhino(screen, 450, 150, eleph, "eleph_3")
-eleph_4 = Rhino(screen, 550, 150, eleph, "eleph_4")
-eleph_5 = Rhino(screen, 650, 150, eleph, "eleph_5")
+eleph_1 = Rhino(screen, 250, 150, eleph, "eleph_1", 180)
+eleph_2 = Rhino(screen, 350, 150, eleph, "eleph_2", 180)
+eleph_3 = Rhino(screen, 450, 150, eleph, "eleph_3", 180)
+eleph_4 = Rhino(screen, 550, 150, eleph, "eleph_4", 180)
+eleph_5 = Rhino(screen, 650, 150, eleph, "eleph_5", 180)
 
 mountain_1 = Montagne(screen, 350, 450, mont, "mountain_1")
 mountain_2 = Montagne(screen, 450, 450, mont, "mountain_2")
