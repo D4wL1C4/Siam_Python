bf580e15d7b400d3c0ce3be4fdf8d344432cd78b
An1one <96392343+An1onegit@users.noreply.github.com>
2023-04-01 17:20:25 +0200

    Update pieces.py

--- siam_game/pieces.py
+++ siam_game/pieces.py
@@ -2,9 +2,6 @@ import pygame
 from siam_game.plateau import *
 from siam_game.ressources import *
 
-global Tour 
-Tour = False
-
 class Rhino:
     previousPos = (0,0)
     newPos = (0,0)
@@ -22,33 +19,28 @@ class Rhino:
         self.forceRhino = 1
 
     def Update(self):
-        global Tour
         mousePos = pygame.mouse.get_pos()
         screen.blit(self.image, (self.posX, self.posY))
         #plateau[i][k] = 1      
-        plateau[int((self.posY - 150)//100)][int((self.posX - 250)//100)] = 1
         #print(int((self.posX - 250)//100), int((self.posY - 150)//100))
-        print(Tour)
         
         
     def select(self):
-        global Tour
         mousePos = pygame.mouse.get_pos()
 
-        if self.rect.collidepoint(mousePos) and self.selected == False and Tour == False:
+        if self.rect.collidepoint(mousePos) and self.selected == False:
             self.previousPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
             print(f"click sur {self.name}")
             self.selected = True
 
 
     def poser(self):
-        global Tour
         mousePos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(mousePos) and self.selected == True:
             self.newPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
             print(plateau[self.newPos[1]][self.newPos[0]])
-            if abs(self.newPos[0] - self.previousPos[0]) <= 1 and abs(self.newPos[1] - self.previousPos[1]) <= 1 and plateau[self.newPos[1]][self.newPos[0]] == 0:
+            if ((abs(self.newPos[0] - self.previousPos[0]) == 1 and abs(self.newPos[1] - self.previousPos[1]) == 0) or (abs(self.newPos[0] - self.previousPos[0]) == 0 and abs(self.newPos[1] - self.previousPos[1]) == 1)) and plateau[self.newPos[1]][self.newPos[0]] == 0:
                 # if self.rotation == 0 and self.newPos[1] == self.previousPos[1] + 1:
                 #     self.selected = False
                 #     self.rect.center = 250, 150
@@ -68,26 +60,66 @@ class Rhino:
                 #     self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
                 #     print(f"reclick sur {self.name}")
                 # elif self.rotation == 180 and self.newPos[1] == self.previousPos[1] - 1:
-                self.selected = False
-                self.rect.center = 250, 150
-                self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
-                self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                self.placeAndSnap()
                 print(f"reclick sur {self.name}")
-                Tour = True
-            elif abs(self.newPos[0] - self.previousPos[0]) <= 1 and abs(self.newPos[1] - self.previousPos[1]) <= 1 and plateau[self.newPos[1]][self.newPos[0]] == 2:
-                for mountain in mountains:
-                    if self.rect.collidepoint(mountain.rect.center):
-                        mountain.posX, mountain.posY = ((self.newPos[0] - self.previousPos[0])*100), self.posY + ((self.newPos[1] - self.previousPos[1])*100)
+            elif ((abs(self.newPos[0] - self.previousPos[0]) == 1 and abs(self.newPos[1] - self.previousPos[1]) == 0) or (abs(self.newPos[0] - self.previousPos[0]) == 0 and abs(self.newPos[1] - self.previousPos[1]) == 1)) and (plateau[self.newPos[1]][self.newPos[0]] == 2 or plateau[self.newPos[1]][self.newPos[0]] == 3 or plateau[self.newPos[1]][self.newPos[0]] == 4):
+                if plateau[self.newPos[1]][self.newPos[0]] == 2:
+                    mountain = mountain_1
+                elif plateau[self.newPos[1]][self.newPos[0]] == 3:
+                    mountain = mountain_2
+                elif plateau[self.newPos[1]][self.newPos[0]] == 4:
+                    mountain = mountain_3
+                if self.newPos[0] - self.previousPos[0] == 1: #deplacement à droite
+                    if plateau[self.newPos[1]][self.newPos[0] + 1] == 0: #verifie si piece à droite de montagne
+                        mountain.posX += 100
                         mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[0] - self.previousPos[0] == -1: #deplacement à gauche
+                    if plateau[self.newPos[1]][self.newPos[0] - 1] == 0: #verifie si piece à gauche de montagne
+                        mountain.posX -= 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[1] - self.previousPos[1] == 1: #deplacement en bas
+                    if plateau[self.newPos[1] + 1][self.newPos[0]] == 0: #verifie si piece en bas de montagne
+                        mountain.posY += 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[1] - self.previousPos[1] == -1: #deplacement en haut
+                    if plateau[self.newPos[1] - 1][self.newPos[0]] == 0: #verifie si piece en haut de montagne
+                        mountain.posY -= 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
 
             else:
-                self.selected = False
-                self.posX = (self.previousPos[0]*100)+250
-                self.posY = (self.previousPos[1]*100)+150
-                self.rect.center = self.posX, self.posY
-                self.newPos = (self.previousPos)
-                print("Else")
-        
+                self.goBack()
+
+    def goBack(self):
+        self.selected = False
+        self.posX = (self.previousPos[0]*100)+250
+        self.posY = (self.previousPos[1]*100)+150
+        self.rect.center = self.posX, self.posY
+        self.newPos = (self.previousPos)
+        print("Else")
+
+    def placeAndSnap(self):
+        mousePos = pygame.mouse.get_pos()
+
+        self.selected = False
+        self.rect.center = 250, 150
+        self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+        self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+        plateau[self.newPos[1]][self.newPos[0]] = 1
+        plateau[self.previousPos[1]][self.previousPos[0]] = 0
+
+
     def move(self, x, y):
         if self.selected == True:
             self.posX = x - square/2
@@ -123,26 +155,23 @@ class Elephant:
         mousePos = pygame.mouse.get_pos()
         screen.blit(self.image, (self.posX, self.posY))
         #plateau[i][k] = 1      
-        plateau[int((self.posY - 150)//100)][int((self.posX - 250)//100)] = 1
         #print(int((self.posX - 250)//100), int((self.posY - 150)//100))
 
     def select(self):
-        global Tour
         mousePos = pygame.mouse.get_pos()
 
-        if self.rect.collidepoint(mousePos) and self.selected == False and Tour == True:
+        if self.rect.collidepoint(mousePos) and self.selected == False:
             self.previousPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
             print(f"click sur {self.name}")
             self.selected = True
         
     def poser(self):
-        global Tour
         mousePos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(mousePos) and self.selected == True:
             self.newPos = (int((mousePos[0] - 250)//100), int((mousePos[1] - 150)//100))
             print(plateau[self.newPos[1]][self.newPos[0]])
-            if abs(self.newPos[0] - self.previousPos[0]) <= 1 and abs(self.newPos[1] - self.previousPos[1]) <= 1 and plateau[self.newPos[1]][self.newPos[0]] == 0:
+            if ((abs(self.newPos[0] - self.previousPos[0]) == 1 and abs(self.newPos[1] - self.previousPos[1]) == 0) or (abs(self.newPos[0] - self.previousPos[0]) == 0 and abs(self.newPos[1] - self.previousPos[1]) == 1)) and plateau[self.newPos[1]][self.newPos[0]] == 0:
                 # if self.rotation == 0 and self.newPos[1] == self.previousPos[1] + 1:
                 #     self.selected = False
                 #     self.rect.center = 250, 150
@@ -162,19 +191,97 @@ class Elephant:
                 #     self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
                 #     print(f"reclick sur {self.name}")
                 # elif self.rotation == 180 and self.newPos[1] == self.previousPos[1] - 1:
-                self.selected = False
-                self.rect.center = 250, 150
-                self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
-                self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+                self.placeAndSnap()
                 print(f"reclick sur {self.name}")
-                Tour = False
+            elif ((abs(self.newPos[0] - self.previousPos[0]) == 1 and abs(self.newPos[1] - self.previousPos[1]) == 0) or (abs(self.newPos[0] - self.previousPos[0]) == 0 and abs(self.newPos[1] - self.previousPos[1]) == 1)) and (plateau[self.newPos[1]][self.newPos[0]] == 2 or plateau[self.newPos[1]][self.newPos[0]] == 3 or plateau[self.newPos[1]][self.newPos[0]] == 4):
+                if plateau[self.newPos[1]][self.newPos[0]] == 2:
+                    mountain = mountain_1
+                elif plateau[self.newPos[1]][self.newPos[0]] == 3:
+                    mountain = mountain_2
+                elif plateau[self.newPos[1]][self.newPos[0]] == 4:
+                    mountain = mountain_3
+                if self.newPos[0] - self.previousPos[0] == 1: #deplacement à droite
+                    if plateau[self.newPos[1]][self.newPos[0] + 1] == 0: #verifie si piece à droite de montagne
+                        mountain.posX += 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[0] - self.previousPos[0] == -1: #deplacement à gauche
+                    if plateau[self.newPos[1]][self.newPos[0] - 1] == 0: #verifie si piece à gauche de montagne
+                        mountain.posX -= 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[1] - self.previousPos[1] == 1: #deplacement en bas
+                    if plateau[self.newPos[1] + 1][self.newPos[0]] == 0: #verifie si piece en bas de montagne
+                        mountain.posY += 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+                if self.newPos[1] - self.previousPos[1] == -1: #deplacement en haut
+                    if plateau[self.newPos[1]][self.newPos[0] - 1] == 0: #verifie si piece en haut de montagne
+                        mountain.posY -= 100
+                        mountain.rect.center = mountain.posX, mountain.posY
+                        self.placeAndSnap()
+                    else:
+                        self.goBack()
+
             else:
-                self.selected = False
-                self.posX = (self.previousPos[0]*100)+250
-                self.posY = (self.previousPos[1]*100)+150
-                self.rect.center = self.posX, self.posY
-                self.newPos = (self.previousPos)
-                print("Else")
+                self.goBack()
+
+    def goBack(self):
+        self.selected = False
+        self.posX = (self.previousPos[0]*100)+250
+        self.posY = (self.previousPos[1]*100)+150
+        self.rect.center = self.posX, self.posY
+        self.newPos = (self.previousPos)
+        print("Else")
+
+    def placeAndSnap(self):
+        mousePos = pygame.mouse.get_pos()
+
+        self.selected = False
+        self.rect.center = 250, 150
+        self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+        self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+        plateau[self.newPos[1]][self.newPos[0]] = 1
+        plateau[self.previousPos[1]][self.previousPos[0]] = 0
+
+
+    def move(self, x, y):
+        if self.selected == True:
+            self.posX = x - square/2
+            self.posY = y - square / 2
+            self.rect.center = x, y
+
+    def rotate(self, degres):
+        mousePos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mousePos):
+            self.image = pygame.transform.rotate(self.image, degres)
+            self.rotation += degres 
+        
+
+    def goBack(self):
+        self.selected = False
+        self.posX = (self.previousPos[0]*100)+250
+        self.posY = (self.previousPos[1]*100)+150
+        self.rect.center = self.posX, self.posY
+        self.newPos = (self.previousPos)
+        print("Else")
+
+    def placeAndSnap(self):
+        mousePos = pygame.mouse.get_pos()
+
+        self.selected = False
+        self.rect.center = 250, 150
+        self.rect.center = 250 + int((mousePos[0] - 250)/100)* square + square/2, 150 + int((mousePos[1] - 150)/100)* square + square/2
+        self.posX, self.posY = self.rect.centerx - square/2, self.rect.centery - square/2
+        plateau[self.newPos[1]][self.newPos[0]] = 1
+        plateau[self.previousPos[1]][self.previousPos[0]] = 0
         
     def move(self, x, y):
         if self.selected == True:
@@ -191,16 +298,17 @@ class Elephant:
 
 
 class Montagne:
-    def __init__(self, _screen, posX : int, posY : int, image, name : str):
+    def __init__(self, _screen, posX : int, posY : int, image, name : str, type : int):
         self.posX = posX
         self.posY = posY
         self.image = pygame.transform.scale(image, (square, square))
         self.rect = self.image.get_rect()
         self.rect.center = posX + square/2, posY + square/2
+        self.type = type
     
     def Update(self):
         screen.blit(self.image, (self.posX, self.posY))
-        plateau[int((self.posY - 150)//100)][int((self.posX - 250)//100)] = 2
+        plateau[int((self.posY - 150)//100)][int((self.posX - 250)//100)] = self.type
 
 
 rhino_1 = Rhino(screen, 250, 750, rhino, "rhino_1", 0)
@@ -215,9 +323,9 @@ eleph_3 = Rhino(screen, 450, 150, eleph, "eleph_3", 180)
 eleph_4 = Rhino(screen, 550, 150, eleph, "eleph_4", 180)
 eleph_5 = Rhino(screen, 650, 150, eleph, "eleph_5", 180)
 
-mountain_1 = Montagne(screen, 350, 450, mont, "mountain_1")
-mountain_2 = Montagne(screen, 450, 450, mont, "mountain_2")
-mountain_3 = Montagne(screen, 550, 450, mont, "mountain_3")
+mountain_1 = Montagne(screen, 350, 450, mont, "mountain_1", 2)
+mountain_2 = Montagne(screen, 450, 450, mont, "mountain_2", 3)
+mountain_3 = Montagne(screen, 550, 450, mont, "mountain_3", 4)
         
 pieces = [rhino_1, rhino_2, rhino_3, rhino_4, rhino_5, eleph_1, eleph_2, eleph_3, eleph_4, eleph_5, mountain_1, mountain_2, mountain_3]  
 pions = [rhino_1, rhino_2, rhino_3, rhino_4, rhino_5, eleph_1, eleph_2, eleph_3, eleph_4, eleph_5]
