9248b6d30dbc093956e50379290ba69106df0e65
An1one <96392343+An1onegit@users.noreply.github.com>
2023-04-03 21:10:42 +0200

    Update plateau.py

--- siam_game/plateau.py
+++ siam_game/plateau.py
@@ -1,16 +1,18 @@
 import pygame
 from siam_game.ressources import *
 
+#version virtuelle du plateau afficher sur la fenêtre
 plateau = [
-        [1,1,1,1,1],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,2,3,4,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [1,1,1,1,1]
+        [0,1,1,1,1,1,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,2,3,4,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,1,1,1,1,1,0]
     ]
 
+#permet de dessiner un plateau de jeu en fonction de 2 couleurs différentes, une position x et y et une taille de case
 def drawPlate(surface, color, colorbis, posX, posY, width, height):
     for i in range(row):
         if i != 0:
@@ -26,14 +28,3 @@ def drawPlate(surface, color, colorbis, posX, posY, width, height):
                 pygame.draw.rect(surface, color, (posX, posY + square*k, width, height))
             else:
                 pygame.draw.rect(surface, colorbis, (posX, posY  + square*k, width, height))
-
-
-    # print("_____Plateau_____")
-    # print(plateau[0])
-    # print(plateau[1])
-    # print(plateau[2])
-    # print(plateau[3])
-    # print(plateau[4])
-    # print(plateau[5])
-    # print(plateau[6])
-    # print("_________________")    
